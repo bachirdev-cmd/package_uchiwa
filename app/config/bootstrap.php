3f0d324bfc7dfa51f3527e3970f2c4f31029d434
Uchiwa75567 <pabassdiame76@gmail.com>
2025-07-24 14:11:51 +0000

    Fix bootstrap for production environment

--- app/config/bootstrap.php
+++ app/config/bootstrap.php
@@ -1,5 +1,14 @@
 <?php
-require_once '../vendor/autoload.php';
-require_once '../app/config/helpers.php';
-require_once '../routes/route.web.php';
-require_once '../app/config/env.php';
+
+// Déterminer le chemin racine selon l'environnement
+$rootPath = __DIR__ . '/../../';
+
+require_once $rootPath . 'vendor/autoload.php';
+
+// Charger les variables d'environnement uniquement si pas dans Docker/Production
+if (!isset($_ENV['DOCKER_ENV']) && !isset($_ENV['RENDER'])) {
+    require_once $rootPath . 'app/config/env.php';
+}
+
+require_once $rootPath . 'app/config/helpers.php';
+require_once $rootPath . 'routes/route.web.php';
